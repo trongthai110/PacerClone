a400e1702b66fe04b1ad2bb6b31d5992fa3e2784
Nguyen Dang Trong Thai <115258405+trongthai110@users.noreply.github.com>
2023-04-21 15:10:51 +0700

    Config Database 21/4

--- Pacer/Scences/Home/Model/HomeModel.swift
+++ Pacer/Scences/Home/Model/HomeModel.swift
@@ -8,22 +8,37 @@
 import Foundation
 import RealmSwift
 
-class Health: Object {
-    @objc dynamic var day: Double = 0
-    var hours = List<HourlyHealth>()
-    convenience init( day: Double) {
-        self.init()
-        self.day = day
-    }
+//class Health: Object {
+//    @objc dynamic var day: Double = 0
+//    var hours = List<HourlyHealth>()
+//    convenience init( day: Double) {
+//        self.init()
+//        self.day = day
+//    }
+//}
+//
+//class HourlyHealth: Object {
+//    @objc dynamic var hour: Int = 0
+//    @objc dynamic var steps: Int = 0
+//
+//    convenience init(hour: Int, steps: Int) {
+//        self.init()
+//        self.hour = hour
+//        self.steps = steps
+//    }
+//}
+
+
+class Step: Object {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var date: Double
+    @Persisted var step: Int
+    @Persisted var steps = List<StepDetail>()
 }
 
-class HourlyHealth: Object {
-    @objc dynamic var hour: Int = 0
-    @objc dynamic var steps: Int = 0
-    
-    convenience init(hour: Int, steps: Int) {
-        self.init()
-        self.hour = hour
-        self.steps = steps
-    }
+class StepDetail: Object {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var date: Double
+    @Persisted var step: Int
+    @Persisted var stepId: Step?
 }
